5cf028c4e416271f77d0f95f2a037ad142b81463
SuperXavier42 <superxavier100@gmail.com>
2026-04-14 20:32:17 +0100

    working on delete task button

--- src/main/java/projects/uah/project_manager/display/ProjectPanel.java
+++ src/main/java/projects/uah/project_manager/display/ProjectPanel.java
@@ -24,7 +24,7 @@ public class ProjectPanel extends JPanel {
      */
     public ProjectPanel(ProjectManager pm, Project project) {
         
-        reloadTasks(project);
+        reloadTasks(pm, project);
         
         setLayout(new BorderLayout());    
         setVisible(true);
@@ -32,13 +32,18 @@ public class ProjectPanel extends JPanel {
         // New button panel
         JPanel buttonPanel = new JPanel(new BorderLayout());
         
-        // Add task button added to the right side of project panel
+        // New button panel on right side of project panel
         JPanel rightBtns = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        
+        // add task button
         JButton addTaskBtn = new JButton("New Task");
         rightBtns.add(addTaskBtn, BorderLayout.EAST);
+        
+        // delete task button
+        JButton delTaskBtn = new JButton("Delete Task");
+        rightBtns.add(delTaskBtn, BorderLayout.SOUTH);
+        
         buttonPanel.add(rightBtns);
-        
-        
         add(buttonPanel, BorderLayout.EAST);
         
         // Creates the panel for the tasks
@@ -64,16 +69,50 @@ public class ProjectPanel extends JPanel {
             } else {
                 project.addTask(new Task(name, "", LocalDate.now(), 1, false));
                 DataManager.save(pm);
-                reloadTasks(project);
+                reloadTasks(pm, project);
             }
-        });   
+        });  
+        
+        // listener for delete button
+        delTaskBtn.addActionListener(e -> {
+            String[] task_names = project.getTasks().stream().map(Task::getName).toArray(String[]::new);
+            if(project.getTasks().isEmpty()){
+                JOptionPane.showMessageDialog(this, "No tasks to delete.");
+                return;
+            }
+            JList<String> task_list = new JList<>(task_names);
+            task_list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+            task_list.setSelectedIndex(0);
+            int select_project = JOptionPane.showConfirmDialog(
+                this,
+                new JScrollPane(task_list),
+                "Select Project to Delete",
+                JOptionPane.OK_CANCEL_OPTION
+            );
+            if(select_project == JOptionPane.OK_OPTION){
+                int double_check = JOptionPane.showConfirmDialog(
+                    this,
+                    "Are you sure?",
+                    "Confirm",
+                    JOptionPane.YES_NO_OPTION
+                );
+                if(double_check == JOptionPane.YES_OPTION){
+                    int index = task_list.getSelectedIndex();
+                    // project.removeTask();
+
+                    DataManager.save(pm); 
+                }
+            }
+        });
+        
+        reloadTasks(pm, project);
     }
     
-    private void reloadTasks(Project project) {
+    private void reloadTasks(ProjectManager pm, Project project) {
         // removes all tasks to add from scratch
         taskListPanel.removeAll();
         for(Task task : project.getTasks()){
-            taskListPanel.add(new TaskPanel(task));
+            taskListPanel.add(new TaskPanel(pm, project, task));
         }
         taskListPanel.revalidate();
         taskListPanel.repaint();
--- src/main/java/projects/uah/project_manager/display/TaskPanel.java
+++ src/main/java/projects/uah/project_manager/display/TaskPanel.java
@@ -3,40 +3,40 @@ package projects.uah.project_manager.display;
 import javax.swing.*;
 import java.awt.*;
 import javax.swing.border.CompoundBorder;
+import projects.uah.project_manager.manager.ProjectManager;
+import projects.uah.project_manager.model.Project;
 import projects.uah.project_manager.model.Task;
 
 public class TaskPanel extends JPanel {
 
     /**
      * Constructs a new TaskPanel and initializes its UI components.
+     * @param pm      project manager to save data
+     * @param project project that the task is part of
      * @param task to display and keep data 
      */
-    public TaskPanel(Task task) {
-    setPreferredSize(new Dimension(200, 300));
-    setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(8,8,8,8),
-        new CompoundBorder(BorderFactory.createLineBorder(Color.GRAY),BorderFactory.createEmptyBorder(8, 8, 8, 8))));
-    setLayout(new BorderLayout());
-    
-    //Create time text added to the task panel
-    JPanel infoPanel = new JPanel(new BorderLayout());
-    infoPanel.add(new JLabel(task.getName()), BorderLayout.WEST);
-    infoPanel.add(new JLabel("Created: " + task.getCreationDate()), BorderLayout.EAST);
-    add(infoPanel, BorderLayout.NORTH);
-    // New button panel
-    JPanel buttonPanel = new JPanel(new BorderLayout());
+    public TaskPanel(ProjectManager pm, Project project, Task task) {
+        setPreferredSize(new Dimension(200, 300));
+        setBorder(new CompoundBorder(BorderFactory.createEmptyBorder(8,8,8,8),
+            new CompoundBorder(BorderFactory.createLineBorder(Color.GRAY),BorderFactory.createEmptyBorder(8, 8, 8, 8))));
+        setLayout(new BorderLayout());
 
-    // Remove task button added to the bottom right side of task panel
-    JPanel rightBtns = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-    JButton removeTaskBtn = new JButton("Delete Task");
-    rightBtns.add(removeTaskBtn, BorderLayout.EAST);
-    buttonPanel.add(rightBtns);
-    add(buttonPanel, BorderLayout.SOUTH);
-    
-    // listener for remove button
-    removeTaskBtn.addActionListener(e -> {
-        System.out.println("Removing Task: " + task.getName());
-        //project.removeTask(task);
-        //maybe https://docs.oracle.com/javase/tutorial/uiswing/components/tabbedpane.html can help?
-    }); 
-}
+        //Create time text added to the task panel
+        JPanel infoPanel = new JPanel(new BorderLayout());
+        infoPanel.add(new JLabel(task.getName()), BorderLayout.WEST);
+        infoPanel.add(new JLabel("Created: " + task.getCreationDate()), BorderLayout.EAST);
+        add(infoPanel, BorderLayout.NORTH);
+        // New button panel
+        JPanel buttonPanel = new JPanel(new BorderLayout());
+
+        // Remove task button added to the bottom right side of task panel
+        JPanel rightBtns = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JButton removeTaskBtn = new JButton("Delete Task");
+        rightBtns.add(removeTaskBtn, BorderLayout.EAST);
+        buttonPanel.add(rightBtns);
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        // listener for remove button
+        
+    }
 }
